b3c317ebee70abfbb1ea557601e88468db2d5e11
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2026-03-01 02:02:27 +0000

    update

--- backend/app/services/predictor_service.py
+++ backend/app/services/predictor_service.py
@@ -20,7 +20,6 @@ def get_model():
         _model = joblib.load(MODEL_PATH)
         print(f"✅ Model loaded. Expected features: {_model.feature_names_in_}")
     return _model
-get_model()  # Load model at startup
 
 
 def build_feature_row(req: PredictRequest) -> pd.DataFrame:
